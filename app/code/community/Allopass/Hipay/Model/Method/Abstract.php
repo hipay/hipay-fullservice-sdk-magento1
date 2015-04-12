1c29419f836b68765c26dfd4efe7e4a2d3bb8937
kassim <kassim@sirateck.com>
2015-04-12 19:21:35 +0200

    --add url to order in cdata1

--- app/code/community/Allopass/Hipay/Model/Method/Abstract.php
+++ app/code/community/Allopass/Hipay/Model/Method/Abstract.php
@@ -824,6 +824,9 @@ abstract class Allopass_Hipay_Model_Method_Abstract extends Mage_Payment_Model_M
 	
 		$params = $this->getCustomerParams($payment,$params);
 		$params = $this->getShippingParams($payment,$params);
+		
+		//add url to order in BO Magento
+		$params['cdata1'] = Mage::getUrl('adminhtml/sales_order/view',array('_secure'=>true,'order_id'=>$payment->getOrder()->getId()));
 	
 	
 		return $params;
