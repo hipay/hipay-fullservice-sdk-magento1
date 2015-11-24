17aca5d3f369a12ef06ca6c14435a3e92d9170a2
Kassim <kassim@sirateck.com>
2015-11-24 22:53:41 +0100

    feat(tools):Create package extension from composer script
    We refactor project architecture also

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+  'extension_name'         => 'Allopass_Hipay',
+  'summary'                => 'Official HiPay Fullservice payment extension.',
+  'description'            => 'HiPay Fullservice is the first payment platform oriented towards merchants that responds to all matters related to online payment: transaction processing, risk management, relationship management with banks and acquirers, financial reconciliation or even international expansion.',
+  'notes'                  => '',
+  'extension_version'      => '0.1.0',
+  'skip_version_compare'   => false,
+  'auto_detect_version'    => true,
+
+  'stability'              => 'stable',
+  'license'                => 'General Public License (GPL)',
+  'channel'                => 'community',
+
+  'author_name'            => 'Kassim Belghait',
+  'author_user'            => 'Sirateck',
+  'author_email'           => 'kassim@sirateck.com',
+
+  'base_dir'               => __DIR__.'/package',
+  'archive_files'          => 'Allopass_Hipay.tar',
+  'path_output'            => __DIR__.'/package',
+
+  'php_min'                => '5.2.0',
+  'php_max'                => '6.0.0',
+
+  'extensions'             => array()
+);
