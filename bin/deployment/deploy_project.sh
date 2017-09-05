25cd9019982c9d3766c2d4f9735846bb6c3712e9
BERTHELOT Aymeric <aberthelot@hipay.com>
2017-09-05 17:34:39 +0200

    Feature/test deploy (#116)
    - Add deployment

--- bin/deployment/deploy_project.sh
+++ bin/deployment/deploy_project.sh
@@ -1,3 +1,5 @@
 port=$(wget --no-check-certificate --user=$DOCKER_MACHINE_LOGIN --password=$DOCKER_MACHINE_PASS -qO- https://docker-knock-auth.hipay.org/KyP54YzX/?srvname=deploy.hipay-pos-platform.com)
 
-sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port curl --user admin:$PASS_JENKINS -s "http://jenkins.hipay-pos-platform.com:8080/job/Repo_test/build?token=${TOKEN_JENKINS}&cause=Deploy+Magento1"
+echo "Deploy project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH"
+sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port  "export DOCKER_API_VERSION=1.23 && docker exec " \
+    "deploy.hipay-pos-platform.com" /deploy/deploy_project.sh  $CIRCLE_PROJECT_REPONAME $CIRCLE_BRANCH
