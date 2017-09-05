3594f24b19fe1ee505edab40e15f3f0628b45297
BERTHELOT AYMERIC <lemaildeaymeric@gmail.com>
2017-09-05 16:31:51 +0200

    Install SSH PASS for deploying

--- bin/deployment/deploy_project.sh
+++ bin/deployment/deploy_project.sh
@@ -1,3 +1,4 @@
 port=$(wget --no-check-certificate --user=$DOCKER_MACHINE_LOGIN --password=$DOCKER_MACHINE_PASS -qO- https://docker-knock-auth.hipay.org/KyP54YzX/?srvname=deploy.hipay-pos-platform.com)
 
-sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port ls
+echo "Deploy project on $CIRCLE_PROJECT_REPONAME $CIRCLE_BRANCH"
+sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port cd /deploy/ && ./deploy_project.sh $CIRCLE_PROJECT_REPONAME $CIRCLE_BRANCH
