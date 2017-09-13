f624b2230c994967addf799ccb4aa92cca89cc1c
BERTHELOT AYMERIC <lemaildeaymeric@gmail.com>
2017-09-13 16:33:35 +0200

    Add build circle in context

--- bin/deployment/deploy_project.sh
+++ bin/deployment/deploy_project.sh
@@ -1,6 +1,7 @@
 port=$(wget --no-check-certificate --user=$DOCKER_MACHINE_LOGIN --password=$DOCKER_MACHINE_PASS -qO- https://docker-knock-auth.hipay.org/KyP54YzX/?srvname=deploy.hipay-pos-platform.com)
 
-BRANCH=${$CIRCLE_BRANCH////-}
+BRANCH=${CIRCLE_BRANCH////-}
+
 echo "Create Artifact project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH to /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH"
 sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port mkdir /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH
 
