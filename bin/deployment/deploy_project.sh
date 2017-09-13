1057e9170a278f2fd2d06c8a591cc26a611e6ece
BERTHELOT AYMERIC <lemaildeaymeric@gmail.com>
2017-09-13 16:30:15 +0200

    Add build circle in context

--- bin/deployment/deploy_project.sh
+++ bin/deployment/deploy_project.sh
@@ -1,10 +1,11 @@
 port=$(wget --no-check-certificate --user=$DOCKER_MACHINE_LOGIN --password=$DOCKER_MACHINE_PASS -qO- https://docker-knock-auth.hipay.org/KyP54YzX/?srvname=deploy.hipay-pos-platform.com)
 
-echo "Create Artifact project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH"
-sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port mkdir /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BRANCH
+BRANCH=${$CIRCLE_BRANCH////-}
+echo "Create Artifact project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH to /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH"
+sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port mkdir /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH
 
 echo "Transfert Artifact project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH"
-sshpass -p $PASS_DEPLOY scp -P $port ./dist/*.tgz root@docker-knock-auth.hipay.org:/deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BRANCH -p $port
+sshpass -p $PASS_DEPLOY scp -P $port ./dist/*.tgz root@docker-knock-auth.hipay.org:/deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH -p $port
 
 echo "Deploy project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH"
 sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port  "export DOCKER_API_VERSION=1.23 && docker exec " \
