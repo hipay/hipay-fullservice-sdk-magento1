c9eaea71c550ea339f2aacb07728c6de83a305da
BERTHELOT AYMERIC <lemaildeaymeric@gmail.com>
2017-09-28 16:00:41 +0200

    Add TAG support

--- bin/deployment/deploy_project.sh
+++ bin/deployment/deploy_project.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 port=$(wget --no-check-certificate --user=$DOCKER_MACHINE_LOGIN --password=$DOCKER_MACHINE_PASS -qO- https://docker-knock-auth.hipay.org/KyP54YzX/?srvname=deploy.hipay-pos-platform.com)
 
-BRANCH=${CIRCLE_BRANCH////-}
+GITHUB_BRANCH=$CIRCLE_BRANCH
+if [ $CIRCLE_TAG != "" ];then
+    GITHUB_BRANCH=$CIRCLE_TAG
+fi
 
-echo "Create Artifact project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH to /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH"
+echo "Create Artifact project for project $CIRCLE_PROJECT_REPONAME and branch $GITHUB_BRANCH to /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH"
 sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port mkdir /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BUILD_NUM
 
-echo "Transfert Artifact project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH"
+echo "Transfert Artifact project for project $CIRCLE_PROJECT_REPONAME and branch $GITHUB_BRANCH"
 sshpass -p $PASS_DEPLOY scp -P $port ./dist/*.tgz root@docker-knock-auth.hipay.org:/deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BUILD_NUM
 
 echo "Deploy project in artifactory"
@@ -14,8 +17,8 @@ sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port  "export D
     "jira-artifactory-pi.hipay-pos-platform.com" /tmp/jfrog rt u /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BUILD_NUM/*.tgz $CIRCLE_PROJECT_REPONAME/snapshot/ \
     --flat=true --user=admin --password=$ARTIFACTORY_PASSWORD --url http://localhost:8081/artifactory/hipay
 
-echo "Deploy project for project $CIRCLE_PROJECT_REPONAME and branch $CIRCLE_BRANCH"
+echo "Deploy project for project $CIRCLE_PROJECT_REPONAME and branch $GITHUB_BRANCH"
 sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port  "export DOCKER_API_VERSION=1.23 && docker exec " \
-    "deploy.hipay-pos-platform.com" /deploy/deploy_project.sh  $CIRCLE_PROJECT_REPONAME $CIRCLE_BRANCH $CIRCLE_BUILD_URL
+    "deploy.hipay-pos-platform.com" /deploy/deploy_project.sh  $CIRCLE_PROJECT_REPONAME $GITHUB_BRANCH $CIRCLE_BUILD_URL
 
 
