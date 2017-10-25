57dd93e6ac005b5158e0b048bda5b869d369a91f
Andrea Zambon <azambon@webgriffe.com>
2017-10-25 14:28:27 +0200

    Ensures that all setup scripts can also be executed together with the magento core setup scripts

---
+++ src/app/code/community/Allopass/Hipay/data/allopass_hipay_setup/data-upgrade-1.0.9-1.1.0.php
@@ -0,0 +1,70 @@
+<?php
+
+$installer = $this;
+
+$installer->startSetup();
+
+$currentVersion = Mage::getVersion();
+if (version_compare($currentVersion, '1.4.2') == 1) {
+
+    $statusTable = $installer->getTable('sales/order_status');
+    $statusStateTable = $installer->getTable('sales/order_status_state');
+    $statusLabelTable = $installer->getTable('sales/order_status_label');
+
+    $statues = array(
+        'authorization_requested' =>
+            array(
+                'label' => 'Authorization Requested',
+                'code' => 'processing',
+                'is_default' => 0
+            ),
+        'expired' => array(
+            'label' => 'Transaction Expired',
+            'code' => 'holded',
+            'is_default' => 0
+        ),
+        'partial_refund' => array(
+            'label' => 'Partial Refund',
+            'code' => 'processing',
+            'is_default' => 0
+        ),
+        'partial_capture' => array(
+            'label' => 'Partial Capture',
+            'code' => 'processing',
+            'is_default' => 0
+        ),
+    );
+
+    try {
+        foreach ($statues as $status => $infos) {
+            $label = $infos['label'];
+            $code = $infos['code'];
+            $is_default = $infos['is_default'];
+
+            //Insert new Status in DB
+            $data[0] = array(
+                'status' => $status,
+                'label' => $label,
+            );
+
+            $installer->getConnection()->insertArray($statusTable, array('status', 'label'), $data);
+            //Insert relation between state and status
+            $data[0] = array(
+                'status' => $status,
+                'state' => $code,
+                'is_default' => $is_default,
+            );
+
+            $installer->getConnection()->insertArray(
+                $statusStateTable,
+                array('status', 'state', 'is_default'),
+                $data
+            );
+        }
+    } catch (Exception $ex) {
+        //Most likely integrity constraint violation. May happen if all setup scripts are run together (including the
+        //ones in the Magento core) on a Magento version at least equal to 1.6.0
+    }
+}
+
+$installer->endSetup();
