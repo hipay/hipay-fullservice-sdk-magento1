173daf62921aaf00120661a42995dc3e8c799b07
BERTHELOT Aymeric <lemaildeaymeric@gmail.com>
2017-10-27 09:56:07 +0200

    Magento version for paralellisme

--- bin/docker/init-environment.sh
+++ bin/docker/init-environment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-VERSION=1.9.2.3
-VERSION_SAMPLE_DATA=1.9
+MAGENTO_VERSION=1.9.2.3
+SAMPLE_DATA_VERSION=1.9
 
 ################################
 # PHP VERSION
@@ -16,18 +16,15 @@ fi
 ### MAGENTO VERSION
 ################################
 if [ "$2" != '' ]; then
-  VERSION=$2
+  MAGENTO_VERSION=$2
 fi
 
 if [ "$3" != '' ]; then
-VERSION_SAMPLE_DATA=$3
+  SAMPLE_DATA_VERSION=$3
 fi
 
-sed -i -e "s/{SAMPLE_DATA_VERSION\}/$VERSION_SAMPLE_DATA/" ./bin/conf/stage/mage.env.sample
-sed -i -e "s/{MAGENTO_VERSION\}/$VERSION/" ./bin/conf/stage/mage.env.sample
+sed -i -e "s/{SAMPLE_DATA_VERSION\}/$SAMPLE_DATA_VERSION/" docker-compose.stage$PHP_VERSION.yml
+sed -i -e "s/{MAGENTO_VERSION\}/$MAGENTO_VERSION/" docker-compose.stage$PHP_VERSION.yml
+
 docker-compose -f docker-compose.yml -f docker-compose.stage$PHP_VERSION.yml build --no-cache
-
-sed -i -e "s/$VERSION_SAMPLE_DATA/{SAMPLE_DATA_VERSION\}/" ./bin/conf/stage/mage.env.sample
-sed -i -e "s/$VERSION/{MAGENTO_VERSION\}/" ./bin/conf/stage/mage.env.sample
-
 docker-compose -f docker-compose.yml -f docker-compose.stage$PHP_VERSION.yml up -d
