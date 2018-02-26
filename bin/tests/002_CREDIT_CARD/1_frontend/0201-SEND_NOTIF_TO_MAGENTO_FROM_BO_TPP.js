05e37a0e8b3644c7abc6e038bf93d54db3d40a98
Berthelot Aymeric <aberthelot@hipay.com>
2018-02-26 17:17:32 +0100

    Update CasperJS for hash

--- bin/tests/002_CREDIT_CARD/1_frontend/0201-SEND_NOTIF_TO_MAGENTO_FROM_BO_TPP.js
+++ bin/tests/002_CREDIT_CARD/1_frontend/0201-SEND_NOTIF_TO_MAGENTO_FROM_BO_TPP.js
@@ -42,7 +42,7 @@ casper.test.begin('Send Notification to Magento from TPP BackOffice via ' + paym
 	casper.gettingData = function(status) {
 		this.echo("Getting data request from details...", "INFO");
 		this.waitUntilVisible('div#fsmodal', function success() {
-			hash = this.fetchText(x('//tr/td/pre[contains(., "Hash")]')).split('\n')[7].split(':')[1].trim();
+			hash = this.fetchText(x('//tr/td/pre[contains(., "Hash")]')).split('\n')[6].split(':')[1].trim();
 			data = this.fetchText('textarea.copy-transaction-message-textarea');
 			try {
 				test.assert(hash.length > 1, "Hash Code captured !");
