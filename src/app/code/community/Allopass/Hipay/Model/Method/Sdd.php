fc7b6afc366d8c3dc36882da4ce68b15194b8a9b
Landais Etienne <elandais@hipay.com>
2018-08-29 18:05:23 +0200

    Remove electronic signature from SEPA

--- src/app/code/community/Allopass/Hipay/Model/Method/Sdd.php
+++ src/app/code/community/Allopass/Hipay/Model/Method/Sdd.php
@@ -54,11 +54,6 @@ class Allopass_Hipay_Model_Method_Sdd extends Allopass_Hipay_Model_Method_Cc
 
     public function initialize($paymentAction, $stateObject)
     {
-        /* @var $payment Mage_Sales_Model_Order_Payment */
-        $payment = $this->getInfoInstance();
-        $order = $payment->getOrder();
-        $customer = Mage::getModel('customer/customer')->load($order->getCustomerId());
-
         return $this;
     }
 
@@ -71,82 +66,34 @@ class Allopass_Hipay_Model_Method_Sdd extends Allopass_Hipay_Model_Method_Cc
 
     public function place($payment, $amount)
     {
-        // check if Electronic Signature
-        $codeElectronicSignature = $this->getConfigData('electronic_signature');
-        if ($codeElectronicSignature > 0) {
-            // if Electronic signature, action hosted			
-            $order = $payment->getOrder();
-            $customer = Mage::getModel('customer/customer')->load($order->getCustomerId());
-            $request = Mage::getModel('hipay/api_request', array($this));
-            $payment->setAmount($amount);
-            $token = null;
-            $gatewayParams = $this->getGatewayParams($payment, $amount, $token);
+        $request = Mage::getModel('hipay/api_request', array($this));
+        $payment->setAmount($amount);
+        $token = $payment->getAdditionalInformation('token');
+        $gatewayParams = $this->getGatewayParams($payment, $amount, $token);
+        $gatewayParams['operation'] = $this->getOperation();
+        $paymentProduct = $this->getCcTypeHipay($payment->getCcType());
 
-            $gatewayParams['authentication_indicator'] = 1;
-            if ($token === null) {
-                $gatewayParams['payment_product'] = $this->getCcTypeHipay($payment->getCcType());;
-                $gatewayParams['operation'] = $this->getOperation();
+        $gatewayParams['payment_product'] = $paymentProduct;
+        $gatewayParams['gender'] = $payment->getAdditionalInformation('cc_gender');
+        $gatewayParams['firstname'] = $payment->getAdditionalInformation('cc_firstname');
+        $gatewayParams['lastname'] = $payment->getAdditionalInformation('cc_lastname');
+        $gatewayParams['recurring_payment'] = 0;
+        $gatewayParams['iban'] = $payment->getAdditionalInformation('cc_iban');
+        $gatewayParams['issuer_bank_id'] = $payment->getAdditionalInformation('cc_code_bic');
+        $gatewayParams['bank_name'] = $payment->getAdditionalInformation('cc_bank_name');
+        $gatewayParams['authentication_indicator'] = 0;
 
-                if (Mage::getStoreConfig('general/store_information/name') != "") {
-                    $gatewayParams['merchant_display_name'] = Mage::getStoreConfig('general/store_information/name');
-                }
-                
-                $this->_debug($gatewayParams);
-                $gatewayResponse = $request->gatewayRequest(
-                    Allopass_Hipay_Model_Api_Request::GATEWAY_ACTION_ORDER,
-                    $gatewayParams,
-                    $payment->getOrder()->getStoreId()
-                );
-                $this->_debug($gatewayResponse->debug());
+        $this->_debug($gatewayParams);
+        $gatewayResponse = $request->gatewayRequest(
+            Allopass_Hipay_Model_Api_Request::GATEWAY_ACTION_ORDER,
+            $gatewayParams,
+            $payment->getOrder()->getStoreId()
+        );
+        $this->_debug($gatewayResponse->debug());
 
-                return $gatewayResponse->getForwardUrl();
-            } else {
-                $gatewayParams['operation'] = $this->getOperation();
-                $gatewayParams['payment_product'] = Mage::getSingleton('customer/session')->getCustomer()->getHipaySddType();
+        $redirectUrl = $this->processResponseToRedirect($gatewayResponse, $payment, $amount);
 
-                $this->_debug($gatewayParams);
-                $gatewayResponse = $request->gatewayRequest(
-                    Allopass_Hipay_Model_Api_Request::GATEWAY_ACTION_ORDER,
-                    $gatewayParams,
-                    $payment->getOrder()->getStoreId()
-                );
-                $this->_debug($gatewayResponse->debug());
-                $redirectUrl = $this->processResponseToRedirect($gatewayResponse, $payment, $amount);
-                return $redirectUrl;
-            }
-        } else {
-            // if not Electronic signature, action API
-            $order = $payment->getOrder();
-            $customer = Mage::getModel('customer/customer')->load($order->getCustomerId());
-            $request = Mage::getModel('hipay/api_request', array($this));
-            $payment->setAmount($amount);
-            $token = $payment->getAdditionalInformation('token');
-            $gatewayParams = $this->getGatewayParams($payment, $amount, $token);
-            $gatewayParams['operation'] = $this->getOperation();
-            $paymentProduct = $this->getCcTypeHipay($payment->getCcType());
-
-            $gatewayParams['payment_product'] = $paymentProduct;
-            $gatewayParams['gender'] = $payment->getAdditionalInformation('cc_gender');
-            $gatewayParams['firstname'] = $payment->getAdditionalInformation('cc_firstname');
-            $gatewayParams['lastname'] = $payment->getAdditionalInformation('cc_lastname');
-            $gatewayParams['recurring_payment'] = 0;
-            $gatewayParams['iban'] = $payment->getAdditionalInformation('cc_iban');
-            $gatewayParams['issuer_bank_id'] = $payment->getAdditionalInformation('cc_code_bic');
-            $gatewayParams['bank_name'] = $payment->getAdditionalInformation('cc_bank_name');
-            $gatewayParams['authentication_indicator'] = 0;
-
-            $this->_debug($gatewayParams);
-            $gatewayResponse = $request->gatewayRequest(
-                Allopass_Hipay_Model_Api_Request::GATEWAY_ACTION_ORDER,
-                $gatewayParams,
-                $payment->getOrder()->getStoreId()
-            );
-            $this->_debug($gatewayResponse->debug());
-
-            $redirectUrl = $this->processResponseToRedirect($gatewayResponse, $payment, $amount);
-
-            return $redirectUrl;
-        }
+        return $redirectUrl;
     }
 
     /**
@@ -161,38 +108,35 @@ class Allopass_Hipay_Model_Method_Sdd extends Allopass_Hipay_Model_Method_Cc
          */
         $errorMsg = '';
         $paymentInfo = $this->getInfoInstance();
-        // check if Electronic signature
-        $codeElectronicSignature = $this->getConfigData('electronic_signature');
-        if ($codeElectronicSignature == 0) {
-            $iban = new Zend_Validate_Iban();
-            if (!$iban->isValid($paymentInfo->getAdditionalInformation('cc_iban'))) {
-                $errorMsg = Mage::helper('payment')->__('Iban is not correct, please enter a valid Iban.');
-            }
 
-            // variable pour la fonction empty
-            $ccFirstName = $paymentInfo->getAdditionalInformation('cc_firstname');
-            $ccLastName = $paymentInfo->getAdditionalInformation('cc_lastname');
-            $ccCodeBic = $paymentInfo->getAdditionalInformation('cc_code_bic');
-            $ccBankName = $paymentInfo->getAdditionalInformation('cc_bank_name');
-            if (empty($ccFirstName)) {
-                $errorMsg = Mage::helper('payment')->__('Firstname is mandatory.');
-            }
+        $iban = new Zend_Validate_Iban();
+        if (!$iban->isValid($paymentInfo->getAdditionalInformation('cc_iban'))) {
+            $errorMsg = Mage::helper('payment')->__('Iban is not correct, please enter a valid Iban.');
+        }
 
-            if (empty($ccLastName)) {
-                $errorMsg = Mage::helper('payment')->__('Lastname is mandatory.');
-            }
+        // variable pour la fonction empty
+        $ccFirstName = $paymentInfo->getAdditionalInformation('cc_firstname');
+        $ccLastName = $paymentInfo->getAdditionalInformation('cc_lastname');
+        $ccCodeBic = $paymentInfo->getAdditionalInformation('cc_code_bic');
+        $ccBankName = $paymentInfo->getAdditionalInformation('cc_bank_name');
+        if (empty($ccFirstName)) {
+            $errorMsg = Mage::helper('payment')->__('Firstname is mandatory.');
+        }
 
-            if (empty($ccCodeBic)) {
-                $errorMsg = Mage::helper('payment')->__('Code BIC is not correct, please enter a valid Code BIC.');
-            }
+        if (empty($ccLastName)) {
+            $errorMsg = Mage::helper('payment')->__('Lastname is mandatory.');
+        }
 
-            if (empty($ccBankName)) {
-                $errorMsg = Mage::helper('payment')->__('Bank name is not correct, please enter a valid Bank name.');
-            }
+        if (empty($ccCodeBic)) {
+            $errorMsg = Mage::helper('payment')->__('Code BIC is not correct, please enter a valid Code BIC.');
+        }
 
-            if ($errorMsg) {
-                Mage::throwException($errorMsg);
-            }
+        if (empty($ccBankName)) {
+            $errorMsg = Mage::helper('payment')->__('Bank name is not correct, please enter a valid Bank name.');
+        }
+
+        if ($errorMsg) {
+            Mage::throwException($errorMsg);
         }
 
         return $this;
