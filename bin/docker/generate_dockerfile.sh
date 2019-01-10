8a9c0e55bc85284869a424d611db74347ccc2f5a
BERTHELOT Aymeric <lemaildeaymeric@gmail.com>
2019-01-10 17:03:47 +0100

    Test php7

--- bin/docker/generate_dockerfile.sh
+++ bin/docker/generate_dockerfile.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env bash
 
 # Generate DockerFile
-cp -f bin/docker/images/php5-6/Dockerfile bin/docker/images/php5-3/Dockerfile
-cp -f bin/docker/images/php5-6/Dockerfile bin/docker/images/php5-4/Dockerfile
-cp -f bin/docker/images/php5-6/Dockerfile bin/docker/images/php7-0/Dockerfile
+cp -f bin/docker/images/php7-1/Dockerfile bin/docker/images/php5-6/Dockerfile
+cp -f bin/docker/images/php7-1/Dockerfile bin/docker/images/php7-2/Dockerfile
 
 # Change the clause FROM
-sed -i -e "s/FROM php:5.6-apache/FROM php:5.4-apache/" bin/docker/images/php5-4/Dockerfile
-sed -i -e "s/PHP_VERSION=5.6/PHP_VERSION=5.4/" bin/docker/images/php5-4/Dockerfile
+sed -i -e "s/FROM php:7.1-apache/FROM php:5.6-apache/" bin/docker/images/php5-6/Dockerfile
+sed -i -e "s/PHP_VERSION=7.1/PHP_VERSION=5.6/" bin/docker/images/php5-6/Dockerfile
 
-sed -i -e "s/FROM php:5.6-apache/FROM php:7.0-apache/"  bin/docker/images/php7-0/Dockerfile
-sed -i -e "s/PHP_VERSION=5.6/PHP_VERSION=7.0/" bin/docker/images/php7-0/Dockerfile
-
-# Change the clause FROM
-sed -i -e "s/FROM php:5.6-apache/php5.3.29-apache/" bin/docker/images/php5-3/Dockerfile
-sed -i -e "s/PHP_VERSION=5.6/PHP_VERSION=5.3/" bin/docker/images/php5-3/Dockerfile
+sed -i -e "s/FROM php:7.1-apache/FROM php:7.2-apache/" bin/docker/images/php5-6/Dockerfile
+sed -i -e "s/PHP_VERSION=7.1/PHP_VERSION=7.2/" bin/docker/images/php7-2/Dockerfile
 
 # Generate docker_compose
-cp -f docker-compose.test.yml docker-compose.test-php5-4.yml
-cp -f docker-compose.test.yml docker-compose.test-php7-0.yml
-cp -f docker-compose.test.yml docker-compose.test-php5-3.yml
+cp -f docker-compose.test.yml docker-compose.test-php5-6.yml
+cp -f docker-compose.test.yml docker-compose.test-php7-2.yml
 cp -f docker-compose.test.yml docker-compose.test-magento18.yml
 
-
-sed -i -e "s/php5-6/php5-4/" docker-compose.test-php5-4.yml
-sed -i -e "s/php5-6/php7-0/" docker-compose.test-php7-0.yml
-sed -i -e "s/php5-6/php5-3/" docker-compose.test-php5-3.yml
+sed -i -e "s/php7-1/php5-6/" docker-compose.test-php5-6.yml
+sed -i -e "s/php7-1/php7-2/" docker-compose.test-php7-2.yml
