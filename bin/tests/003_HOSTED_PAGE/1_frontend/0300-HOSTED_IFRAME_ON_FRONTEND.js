a7c068b4f4f67744e55573b7f23ac5f8de67a4ca
Landais Etienne <elandais@hipay.com>
2019-04-17 10:53:18 +0200

    Add casper tests

--- bin/tests/003_HOSTED_PAGE/1_frontend/0300-HOSTED_IFRAME_ON_FRONTEND.js
+++ bin/tests/003_HOSTED_PAGE/1_frontend/0300-HOSTED_IFRAME_ON_FRONTEND.js
@@ -4,58 +4,56 @@
  *
  *  To launch test, please pass two arguments URL (BASE URL)  and TYPE_CC ( CB,VI,MC )
  *
-/**********************************************************************************************/
+ /**********************************************************************************************/
 
 var paymentType = "HiPay Enterprise Hosted Page",
     currentBrandCC = typeCC;
 
-casper.test.begin('Test Checkout ' + paymentType + ' with Iframe and ' + currentBrandCC, function(test) {
-	phantom.clearCookies();
+casper.test.begin('Test Checkout ' + paymentType + ' with Iframe and ' + currentBrandCC, function (test) {
+    phantom.clearCookies();
 
-	casper.start(baseURL + "admin/")
+    casper.start(baseURL + "admin/")
     /* Active Hosted payment method with display iframe */
-    .then(function() {
-        this.logToBackend();
-        method.proceed(test, paymentType, "hosted", ['select[name="groups[hipay_hosted][fields][display_iframe][value]"]', '1']);
-    })
-    .thenOpen(baseURL, function() {
-        this.waitUntilVisible('div.footer', function success() {
-            this.selectItemAndOptions();
-        }, function fail() {
-            test.assertVisible("div.footer", "'Footer' exists");
-        }, 10000);
-    })
-    .then(function() {
-        this.addItemGoCheckout();
-    })
-    .then(function() {
-        this.checkoutMethod();
-    })
-    .then(function() {
-        this.billingInformation();
-    })
-    .then(function() {
-        this.shippingMethod();
-    })
-    .then(function() {
-        this.choosingPaymentMethod("method_hipay_hosted");
-    })
-    .then(function() {
-        this.orderReview(paymentType);
-    })
-    /* Fill payment formular inside iframe */
-    .then(function() {
-    	this.wait(10000, function() {
-			this.withFrame(0, function() {
+        .then(function () {
+            this.logToBackend();
+            method.proceed(test, paymentType, "hosted", ['select[name="groups[hipay_hosted][fields][display_iframe][value]"]', '1']);
+        })
+        .thenOpen(baseURL, function () {
+            this.waitUntilVisible('div.footer', function success() {
+                this.selectItemAndOptions();
+            }, function fail() {
+                test.assertVisible("div.footer", "'Footer' exists");
+            }, 10000);
+        })
+        .then(function () {
+            this.addItemGoCheckout();
+        })
+        .then(function () {
+            this.checkoutMethod();
+        })
+        .then(function () {
+            this.billingInformation();
+        })
+        .then(function () {
+            this.shippingMethod();
+        })
+        .then(function () {
+            this.choosingPaymentMethod("method_hipay_hosted");
+        })
+        .then(function () {
+            this.orderReview(paymentType);
+        })
+        /* Fill payment formular inside iframe */
+        .then(function () {
+            this.wait(10000, function () {
                 this.echo("Fill payment Formular", "INFO");
-                this.fillCCFormular(currentBrandCC);
-			});
-    	});
-    })
-    .then(function() {
-        this.orderResult(paymentType);
-    })
-    .run(function() {
-        test.done();
-    });
+                this.fillCCFormular(currentBrandCC, true);
+            });
+        })
+        .then(function () {
+            this.orderResult(paymentType);
+        })
+        .run(function () {
+            test.done();
+        });
 });
